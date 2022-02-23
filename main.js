233c9df250de10f030bc3a6eed21faf8b9b2d620
David Drummer <david.b.drummer@gmail.com>
2022-02-22 20:51:53 -0500

    commit tree 3d42a09b922178c4b85d2991a91243a9795cd8b9

--- main.js
+++ main.js
@@ -2,10 +2,43 @@
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
-// Your JavaScript code goes here!
+//create 'click' event listeners for all hearts
 
+const clickHearts = document.querySelectorAll('.like-glyph')
 
+for (let hearts of clickHearts) {
+  hearts.addEventListener('click', handleClick);
+};
 
+//create handleClick function to determine what happens when the user clicks a heart
+
+function handleClick(event) {
+  mimicServerCall()
+  .then(() => successHeart(event))
+  .catch(resp => failHeart(resp));
+}
+
+//create successHeart function to determine what happens whent the server returns a success
+
+function successHeart(event){
+  let heartEmoji = event.target
+  if (heartEmoji.innerText === EMPTY_HEART) {
+    heartEmoji.innerText = FULL_HEART
+    heartEmoji.classList.add('activated-heart')
+  } else if (heartEmoji.innerText === FULL_HEART) {
+    heartEmoji.innerText = EMPTY_HEART
+    heartEmoji.classList.remove('activated-heart');
+  };
+};
+
+//create failHeart function to determine what happens when the server returns a failure
+
+function failHeart(resp) {
+  errorMessage = document.querySelector('#modal')
+  errorMessage.classList.remove('hidden')
+  errorMessage.querySelector('h2').innerText = `Error! ${resp}`
+  setTimeout(() => errorMessage.classList.add('hidden'), 3000);
+};
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
